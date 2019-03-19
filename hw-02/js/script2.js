cef8d974ea2fb8ec27e079933e95bf207f40cc7e
Sergii.Pushkovskyi <spushkovskiy@gmail.com>
2019-03-19 11:46:09 +0200

    Started working on hw-02. Made additional task

---
+++ hw-02/js/script2.js
@@ -0,0 +1,21 @@
+const passwords = ['qwerty', '111qwe', '123123', 'r4nd0mp4zzw0rd'];
+let attempts = 3;
+let isLogged=false;
+do {
+
+    let pass = prompt("Enter password");
+    for (password of passwords) {
+        if (pass === password) {
+            alert('Добро пожаловать!');
+            isLogged=true;
+            break;
+        }
+    }
+    if (isLogged){
+        break;
+    }
+
+    attempts--;
+    let msg = attempts > 0?`Неверный пароль, у вас осталось ${attempts} попыток`:'У вас закончились попытки, аккаунт заблокирован!'
+    alert(msg);
+} while (isLogged===false &&attempts > 0);
