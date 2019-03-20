fd94504d36bcbe72731a555ac594e482006bbc77
Sergii.Pushkovskyi <spushkovskiy@gmail.com>
2019-03-20 08:16:52 +0200

    Fixed hw-02 additional task. on Cancel button

--- hw-02/js/script2.js
+++ hw-02/js/script2.js
@@ -2,22 +2,26 @@ const passwords = ["qwerty", "111qwe", "123123", "r4nd0mp4zzw0rd"];
 let attempts = 3;
 let isLogged = false;
 do {
-  let pass = prompt("Enter password");
-  for (const password of passwords) {
-    if (pass === password) {
-      alert("Добро пожаловать!");
-      isLogged = true;
-      break;
-    }
-  }
-  if (isLogged) {
-    break;
-  }
+    let pass = prompt("Enter password");
 
-  attempts--;
-  let msg =
-    attempts > 0
-      ? `Неверный пароль, у вас осталось ${attempts} попыток`
-      : "У вас закончились попытки, аккаунт заблокирован!";
-  alert(msg);
+    if (pass === null) {
+        alert("Отменено пользователем");
+        break;
+    }
+    for (const password of passwords) {
+        if (pass === password) {
+            alert("Добро пожаловать!");
+            isLogged = true;
+            break;
+        }
+    }
+    if (isLogged) {
+        break;
+    }
+    attempts--;
+    let msg =
+        attempts > 0
+            ? `Неверный пароль, у вас осталось ${attempts} попыток`
+            : "У вас закончились попытки, аккаунт заблокирован!";
+    alert(msg);
 } while (isLogged === false && attempts > 0);
