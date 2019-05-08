5f9139b6f623dc31aa30b3cbd06381171d4a8489
Sergii.Pushkovskyi <spushkovskiy@gmail.com>
2019-05-08 09:25:19 +0300

    Finished news reader

--- playground/scripts/script.js
+++ playground/scripts/script.js
@@ -1,46 +1,40 @@
 import publications from "./publications.js";
 
-class Reader {
-  constructor(publications) {
-    this.pubs = publications;
-    this.startCount = 0;
-    this.maxCount = publications.length;
-  }
-
-  getCurrentPublication() {
-    console.log(this.pubs[this.startCount]);
-    return this.pubs[this.startCount];
-  }
-
-  getNextPublication() {
-    this.startCount += 1;
-    console.log(this.startCount);
-  }
-
-  gerPreviousPublication() {
-    this.startCount -= 1;
-    console.log(this.pubs[this.startCount]);
-    return this.pubs[this.startCount];
-  }
-}
-
-const pubs = new Reader(publications);
-
 const title = document.querySelector(".card-title");
-title.textContent = pubs.getCurrentPublication().title;
-
 const content = document.querySelector(".content");
-content.textContent = pubs.getCurrentPublication().text;
-
 const counter = document.querySelector(".counter");
-counter.textContent = `${pubs.startCount + 1}/${pubs.maxCount}`;
-
 const prevButton = document.querySelector(".left");
 const nextButton = document.querySelector(".right");
 
-prevButton.addEventListener("click", pubs.gerPreviousPublication.bind(pubs));
-prevButton.addEventListener("click", function() {
-  return (counter.textContent = pubs.startCount);
+let articleCounter = 0;
+let maxValue = publications.length - 1;
+
+const onNextClick = () => {
+  if (articleCounter < maxValue) {
+    articleCounter += 1;
+  }
+};
+
+const onPrevClick = () => {
+  if (articleCounter > 0) {
+    articleCounter -= 1;
+  }
+};
+
+const render = () => {
+  title.textContent = publications[articleCounter].title;
+  content.textContent = publications[articleCounter].text;
+  counter.textContent = `${articleCounter + 1}/${maxValue + 1}`;
+};
+
+document.addEventListener("DOMContentLoaded", render());
+
+nextButton.addEventListener("click", function() {
+  onNextClick();
+  render();
 });
 
-nextButton.addEventListener("click", pubs.getNextPublication.bind(pubs));
+prevButton.addEventListener("click", function() {
+  onPrevClick();
+  render();
+});
