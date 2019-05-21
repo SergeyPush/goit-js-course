f8b05953fcf96aee7eb3936c1967407e7c0d235e
Sergey Pushkovskiy <push_s@mail.ua>
2019-05-21 12:35:02 +0300

    Finished work on hw-08. Ready to submit

--- hw-08/js/script.js
+++ hw-08/js/script.js
@@ -1,3 +1,4 @@
+// Imports and variables declaration
 import QuizData from "./quizdata.js";
 import renderForm from "./QuizForm.js";
 
@@ -11,47 +12,62 @@ const modalHeader = document.querySelector(".modalHeader");
 const closeModal = document.querySelector(".close");
 const { title, questions } = QuizData;
 
+const data = {};
 let correctAnswers = 0;
 let incorrectAnswers = 0;
 let questionNumber = 0;
 let numberOfQuestions = questions.length;
+let testResult;
 
+//Render initial data
 const formEntry = renderForm(questions);
-
 headerTitle.textContent = title;
 questionssection.innerHTML = formEntry;
 
+// Two event listeners
 submitButton.addEventListener("click", onButtonSubmit);
+form.addEventListener("click", handleFormClick);
 
+//Function that listens to input change. Blocks Check button
+function handleFormClick(event) {
+  const target = event.target;
+  if (target.tagName === "INPUT") {
+    const formData = new FormData(form);
+
+    formData.forEach((value, name) => {
+      data[name] = value;
+
+      const answers = Object.values(data);
+      questionNumber = answers.length;
+
+      if (numberOfQuestions !== questionNumber) {
+        submitButton.disabled = true;
+      } else {
+        submitButton.disabled = false;
+      }
+    });
+  }
+}
+
+// Handles Submit button
 function onButtonSubmit(event) {
   event.preventDefault();
-  console.dir(form.elements);
-  const data = {};
-  const formData = new FormData(form);
-
-  formData.forEach((value, name) => {
-    data[name] = value !== null ? value : -1;
-  });
 
   closeModal.addEventListener("click", onCloseModal);
   compareResults(data);
-  // modal.style.display = "block";
+  modal.style.display = "block";
 }
 
+//Closes modal display
 function onCloseModal() {
   modal.style.display = "none";
 }
 
+// Compares actual results with initial test data
 function compareResults(data) {
   const answers = Object.values(data);
-  console.log(answers);
-
   correctAnswers = 0;
   incorrectAnswers = 0;
-  console.log(numberOfQuestions);
-
-  questionNumber = answers.length;
-  console.log(questionNumber);
 
   answers.forEach((item, i) => {
     if (Number(item) === questions[i].answer) {
@@ -61,26 +77,22 @@ function compareResults(data) {
     }
   });
 
+  // Convert Answers to percents
   const toPercentage = item => {
     return Math.floor((item / questionNumber) * 100);
   };
 
-  let testResult;
-
   if (toPercentage(correctAnswers) < 80) {
     testResult = "Тест не пройден";
-    document.querySelector(".modal-header").style.backgroundColor = "red";
+    document.querySelector(".modal-header").style.backgroundColor = "tomato";
   } else {
     testResult = "Тест пройден";
+    document.querySelector(".modal-header").style.backgroundColor = "limeGreen";
   }
 
   modalHeader.textContent = testResult;
   modalContent.textContent = `Правильных ответов: ${toPercentage(
     correctAnswers
-  )} \nНеправильных ответов: ${toPercentage(incorrectAnswers)}
+  )}% \nНеправильных ответов: ${toPercentage(incorrectAnswers)}%
     `;
 }
-
-// Необходимо динамически создать разметку формы по этим данным. Каркас формы + кнопка "Проверить" пусть изначально будут в HTML-документе.
-// При сабмите формы проверь на сколько вопросов пользователь ответил верно и выведи (можно под формой) сообщение об успехе или неудаче. Успешным прохождение теста считается при 80% или более верных ответов.
-// Кнопка "Проверить" должна быть не активна (disabled) до тех пор, пока пользователь не ответит на все вопросы.
